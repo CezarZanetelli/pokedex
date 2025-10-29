1300bdfd467e36669bfce874eb828f81eb576342
Cezar Augusto Zanetelli <cezinhabm@hotmail.com>
2025-10-29 16:15:57 -0300

    Atualização automática

---
+++ src/lib/types/pokemon.ts
@@ -0,0 +1,41 @@
+// src/lib/types/pokemon.ts
+export interface Pokemon {
+  id: number
+  name: string
+  height: number
+  weight: number
+  sprites: {
+    front_default: string
+    other: {
+      'official-artwork': {
+        front_default: string
+      }
+    }
+  }
+  types: Array<{
+    type: {
+      name: string
+    }
+  }>
+  stats: Array<{
+    base_stat: number
+    stat: {
+      name: string
+    }
+  }>
+  abilities: Array<{
+    ability: {
+      name: string
+    }
+  }>
+}
+
+export interface PokemonListResponse {
+  count: number
+  next: string | null
+  previous: string | null
+  results: Array<{
+    name: string
+    url: string
+  }>
+}
